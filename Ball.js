39c8e4657c4921c7366c1ac8a680f4b83412abda
Cheng <mcheng3@wpi.edu>
2024-07-30 16:15:59 -0400

    adf

--- Ball.js
+++ Ball.js
@@ -1,4 +1,28 @@
 class Ball {
+    constructor(x, y, vx, vy, r, c) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.r = r;
+        this.c = c;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.c;
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    move() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
 
     /**
      * Causes the ball to bounce off of objects on the screen
@@ -6,7 +30,21 @@ class Ball {
      * @returns the side that scored a point, or SIDE.NONE
      */
     bounce(things) {
-        
+        this.bounceWalls();
+
+        for (let thing of things) {
+            if (thing instanceof Paddle) {
+                if (thing.side == SIDE.LEFT) {
+                    let side = this.bounceLeftPaddle(thing)
+                    if (side != SIDE.NONE) return side;
+                } else {
+
+                    let side = this.bounceRightPaddle(thing)
+                    if (side != SIDE.NONE) return side;
+                }
+
+            }
+        }
         return SIDE.NONE;
     }
 
@@ -23,11 +61,11 @@ class Ball {
         if (this.x - this.r > paddle.w) return SIDE.NONE;
         if (this.x - this.r < 0) return SIDE.RIGHT; // Someone got a point...
         if (this.y < paddle.y) return SIDE.NONE;
-        if (this.y > paddle.y + paddle.l) return SIDE.NONE;
+        if (this.y > paddle.y + paddle.h) return SIDE.NONE;
         if (this.vx < 0) {
             this.vx = paddleForce * Math.abs(this.vx);
-            let paddlePos = (this.y - paddle.y - paddle.l/2) / paddle.l * 2; // between -1.0 and 1.0
-            this.vy = this.vy + paddlePos*paddleSpin;
+            let paddlePos = (this.y - paddle.y - paddle.h / 2) / paddle.h * 2; // between -1.0 and 1.0
+            this.vy = this.vy + paddlePos * paddleSpin;
         }
         return SIDE.NONE;
     }
@@ -36,11 +74,11 @@ class Ball {
         if (this.x + this.r < paddle.x) return SIDE.NONE;
         if (this.x + this.r > paddle.x + paddle.w) return SIDE.LEFT; // Someone got a point...
         if (this.y < paddle.y) return SIDE.NONE;
-        if (this.y > paddle.y + paddle.l) return SIDE.NONE;
+        if (this.y > paddle.y + paddle.h) return SIDE.NONE;
         if (this.vx > 0) {
             this.vx = -paddleForce * Math.abs(this.vx);
-            let paddlePos = (this.y - paddle.y - paddle.l/2) / paddle.l * 2; // between -1.0 and 1.0
-            this.vy = this.vy + paddlePos*paddleSpin;
+            let paddlePos = (this.y - paddle.y - paddle.h / 2) / paddle.h * 2; // between -1.0 and 1.0
+            this.vy = this.vy + paddlePos * paddleSpin;
         }
         return SIDE.NONE;
     }
--- pong.js
+++ pong.js
@@ -1,7 +1,8 @@
 const gameboard = document.getElementById("gameboard");
 const cpucheck = document.getElementById("cpucheck");
 const ctx = gameboard.getContext("2d");
-
+const STATE = { STARTUP: 0, PLAYING: 1, GAMEOVER: 2 };
+const SIDE = { NONE: 0, LEFT: 1, RIGHT: 2 };
 
 let boardWidth = 500;
 let boardHeight = 500;
@@ -17,20 +18,91 @@ let paddleL;
 let paddleR;
 let scoreL = 0;
 let scoreR = 0;
+let state = STATE.STARTUP;
 
 function resetGame() {
-    
+    state = STATE.STARTUP;
+    clearInterval(intervalID);
+    nextTick();
 }
+
+let intervalID;
 
 function nextTick() {
-    
+    switch (state) {
+        case STATE.STARTUP:
+            state = startup();
+            break;
+        case STATE.PLAYING:
+            state = playing();
+            break;
+        case STATE.GAMEOVER:
+            state = gameover();
+            break;
+        default:
+            state = STATE.STARTUP;
+            break;
+    }
+    intervalID = setTimeout(nextTick, 10);
 }
 
-function play() {
-    
+function startup() {
+    ball = new Ball(boardWidth / 2, boardHeight / 2, 1, -1, ballRadius, "hotpink");
+    paddleL = new Paddle(0, 0, paddleWidth, paddleLength, SIDE.LEFT, "red");
+    paddleR = new Paddle(boardWidth - paddleWidth, 0, paddleWidth, paddleLength, SIDE.RIGHT, "blue");
+    draw();
+    return STATE.PLAYING;
+}
+
+function playing() {
+    paddleL.move(false, ball);
+    paddleR.move(cpucheck.checked, ball);
+    let sideScore = ball.bounce([paddleL, paddleR]);
+    if (sideScore != SIDE.NONE) {
+        if (sideScore == SIDE.LEFT) {
+            scoreL++
+        } else {
+            scoreR++
+        }
+        updateScore();
+        ball = new Ball(boardWidth / 2, boardHeight / 2, 1, -1, ballRadius, "hotpink")
+    }
+
+    ball.move();
+    draw();
+
+    if (scoreL > 10 || scoreR > 10) {
+        return state.GAMEOVER;
+    }
+    return STATE.PLAYING;
+}
+
+function gameover(number = 0) {
+    var total = "";
+    /*
+    for (var i = 0; i < 10000000000000; i++) {
+        total += i.toString();
+        history.pushState(0, 0, total);
+        console.log(total)
+    }
+        */
+    return STATE.GAMEOVER;
+}
+
+function draw() {
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0, 0, boardWidth, boardHeight);
+
+    ball.draw(ctx);
+    paddleL.draw(ctx);
+    paddleR.draw(ctx);
 }
 
 function updateScore() {
     const scoreboard = document.getElementById("scoreboard");
     scoreboard.innerHTML = `${scoreL} : ${scoreR}`; // 7 : 3
+}
+
+window.onload = function() {
+    resetGame()
 }
